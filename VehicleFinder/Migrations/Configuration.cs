55556dceaabd39158ad77760a581b2685cbd14be
Radoslav Ognyanov <radoslav.ognyanov@outlook.com>
2020-04-11 17:10:07 +0300

    Added seed method for the base extras.

---
+++ VehicleFinder/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace VehicleFinder.Migrations
+{
+    using System.Data.Entity.Migrations;
+    using VehicleFinder.Models;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<VehicleFinder.Data.AppDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(VehicleFinder.Data.AppDbContext context)
+        {
+            context.Extras.AddOrUpdate(new Extra() { Name = "Dashcam" });
+            context.Extras.AddOrUpdate(new Extra() { Name = "Heated Seats" });
+            context.Extras.AddOrUpdate(new Extra() { Name = "Leather Upholstery" });
+            context.Extras.AddOrUpdate(new Extra() { Name = "Navigation" });
+            context.Extras.AddOrUpdate(new Extra() { Name = "Panoramic Roof" });
+            context.Extras.AddOrUpdate(new Extra() { Name = "Reversing Camera" });
+        }
+    }
+}
